708971a37a5ac2388a6cc8e28f7f642cbc20b88d
Piers14 <61315972+piers-hinds@users.noreply.github.com>
2022-07-10 19:23:12 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='amex',
+    url='https://github.com/piers-hinds/amex',
+    author='Piers Hinds',
+    author_email='pmxph7@nottingham.ac.uk',
+    packages=['amex'],
+    install_requires=['numpy'],
+    version=0.1,
+)
